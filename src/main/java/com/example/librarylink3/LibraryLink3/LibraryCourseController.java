4de2dca4ec8ba4a21d0e3ce40c7ce1adb44e04ba
podgek92 <p.keane14@universityofgalway.ie>
2024-07-08 21:34:34 +0100

    LibraryCourse added and manage page and edit page. Need to make delete functional and add user end of librarycourses. cannot call Class as reserved word.

---
+++ src/main/java/com/example/librarylink3/LibraryLink3/LibraryCourseController.java
@@ -0,0 +1,41 @@
+package com.example.librarylink3.LibraryLink3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LibraryCourseController {
+
+    @Autowired
+    private LibraryCourseService libraryCourseService;
+
+    @GetMapping("/admin/manage_courses")
+    public String manageCourses(Model model) {
+        model.addAttribute("courses", libraryCourseService.findAllCourses());
+        return "course_list";
+    }
+
+    @GetMapping("/admin/edit_course")
+    public String showEditCoursePage(@RequestParam("id") Long id, Model model) {
+        LibraryCourse course = libraryCourseService.findCourseById(id);
+        model.addAttribute("course", course);
+        return "edit_course";
+    }
+
+    @PostMapping("/admin/update_course")
+    public String updateCourse(@ModelAttribute LibraryCourse course) {
+        libraryCourseService.updateCourse(course);
+        return "redirect:/admin/manage_courses";
+    }
+
+    @PostMapping("/admin/delete_course")
+    public String deleteCourse(@RequestParam("id") Long id) {
+        libraryCourseService.deleteCourse(id);
+        return "redirect:/admin/manage_courses";
+    }
+}
---
+++ src/main/java/com/example/librarylink3/LibraryLink3/LibraryCourseService.java
@@ -0,0 +1,36 @@
+package com.example.librarylink3.LibraryLink3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LibraryCourseService {
+
+    @Autowired
+    private LibraryCourseRepository libraryCourseRepository;
+
+    public LibraryCourse findCourseById(Long id) {
+        Optional<LibraryCourse> courseOpt = libraryCourseRepository.findById(id);
+        return courseOpt.orElse(null);
+    }
+
+    public void updateCourse(LibraryCourse course) {
+        libraryCourseRepository.save(course);
+    }
+
+    public void deleteCourse(Long id) {
+        libraryCourseRepository.deleteById(id);
+    }
+
+    public List<LibraryCourse> findAllCourses() {
+        return libraryCourseRepository.findAll();
+    }
+
+    public List<LibraryCourse> getAllCourses() {
+        return libraryCourseRepository.findAll();
+    }
+
+}
