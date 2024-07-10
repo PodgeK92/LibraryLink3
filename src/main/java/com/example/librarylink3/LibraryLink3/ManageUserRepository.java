d7e5fe8731cc91e81f820988861b010f885d40fa
podgek92 <p.keane14@universityofgalway.ie>
2024-07-10 22:03:11 +0100

    Admin Mange users page

---
+++ src/main/java/com/example/librarylink3/LibraryLink3/ManageUserRepository.java
@@ -0,0 +1,7 @@
+package com.example.librarylink3.LibraryLink3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ManageUserRepository extends JpaRepository<User, String> {
+}
+
