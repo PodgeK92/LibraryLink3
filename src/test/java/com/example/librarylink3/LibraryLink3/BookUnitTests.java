5792cbea236ba28b5b466db38479605c61f3657a
MT-byte <m.dhakan1@nuigalway.ie>
2024-08-04 17:39:29 +0100

    updated all the Book tests

--- src/test/java/com/example/librarylink3/LibraryLink3/BookUnitTests.java
+++ src/test/java/com/example/librarylink3/LibraryLink3/BookUnitTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -14,7 +16,7 @@ public class BookUnitTests {
     private BookService bookService;
 
     @Test
-    public void testAddNewBook() {
+    public void testAddBook() {
         String isbn = "9780553902549";
         String title = "Journey to the Centre of the Earth";
         String author = "Jules Verne";
@@ -29,4 +31,29 @@ public class BookUnitTests {
         assertTrue(retrievedBook.get().getAuthor().contains(author));
     }
 
+
+    @Test
+    public void testUpdateBook() {
+        String isbn = "9780553902549";
+        int correctPublishedYear = 2014;
+        Optional<Book> bookToBeUpdatedOptional = bookService.getBookById(isbn);
+        Book bookToBeUpdated = bookToBeUpdatedOptional.get();
+        bookToBeUpdated.setYearPublished(correctPublishedYear);
+
+        bookService.updateBook(bookToBeUpdated);
+        Optional<Book> retrievedBook = bookService.getBookById(isbn);
+
+        assertEquals(retrievedBook.get().getYearPublished(), correctPublishedYear);
+    }
+
+
+    @Test
+    public void testDeleteBook() {
+        String isbn = "9780553902549";
+
+        bookService.deleteBook(isbn);
+        Optional<Book> retrievedBook = bookService.getBookById(isbn);
+
+        assertTrue(retrievedBook.isEmpty());
+    }
 }
